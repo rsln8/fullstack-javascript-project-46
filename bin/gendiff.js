215b3d1db776a1ff29843ba013c142f94865b0e5
rsln8 <iamrsln23@my.com>
2025-12-17 01:34:11 +0300

    Fix eslint style issues

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,34 +1,22 @@
 #!/usr/bin/env node
 
-import { Command } from 'commander';
-import genDiff from '../src/index.js';
+import { Command } from 'commander'
+import genDiff from '../src/index.js'
 
-const program = new Command();
+const program = new Command()
 
 program
   .name('gendiff')
-  .usage('[options] <filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0')
-  .option('-f, --format [type]', 'output format')
-  .allowExcessArguments(true) 
-  .parse(process.argv);
+  .version('0.0.1')
 
-const options = program.opts();
-const args = program.args;
+program
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .action((filepath1, filepath2, options) => {
+    const diff = genDiff(filepath1, filepath2, options.format)
+    console.log(diff)
+  })
 
-if (args.length !== 2) {
-  console.error('Error: Expected 2 file paths');
-  program.help();
-}
-
-const [filepath1, filepath2] = args;
-const format = options.format || 'stylish';
-
-try {
-  const diff = genDiff(filepath1, filepath2, format);
-  console.log(diff);
-} catch (error) {
-  console.error('Error:', error.message);
-  process.exit(1);
-}
+program.parse()
--- src/parsers.js
+++ src/parsers.js
@@ -1,16 +1,15 @@
-// src/parsers.js
-import yaml from 'js-yaml';
+import yaml from 'js-yaml'
 
 const parse = (data, format) => {
   switch (format) {
     case 'json':
-      return JSON.parse(data);
+      return JSON.parse(data)
     case 'yml':
     case 'yaml':
-      return yaml.load(data);
+      return yaml.load(data)
     default:
-      throw new Error(`Unknown format: ${format}`);
+      throw new Error(`Unknown format: ${format}`)
   }
-};
+}
 
-export default parse;
+export default parse
