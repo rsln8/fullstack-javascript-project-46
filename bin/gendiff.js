0f8ec91be639711e05ba19cb87f01049f9de55fc
rsln8 <iamrsln23@my.com>
2026-02-26 23:03:57 +0300

    feat: add arguments and format option to gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,6 +8,8 @@ program
   .name("gendiff")
   .description("Compares two configuration files and shows a difference.")
   .version("0.0.1", "-V, --version", "output the version number")
+  .arguments("<filepath1> <filepath2>")
+  .option("-f, --format [type]", "output format")
   .helpOption("-h, --help", "display help for command");
 
 program.parse(process.argv);
