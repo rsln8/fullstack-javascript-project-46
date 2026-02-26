90d8cc859b62bc5d81c41ad35bcfd7b832411425
rsln8 <iamrsln23@my.com>
2026-02-26 23:05:57 +0300

    feat: add file reading and parsing

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-
 import { Command } from "commander";
+import genDiff from "../src/index.js";
 
 const program = new Command();
 
@@ -10,6 +10,8 @@ program
   .version("0.0.1", "-V, --version", "output the version number")
   .arguments("<filepath1> <filepath2>")
   .option("-f, --format [type]", "output format")
-  .helpOption("-h, --help", "display help for command");
+  .action((path1, path2) => {
+    genDiff(path1, path2);
+  });
 
 program.parse(process.argv);
