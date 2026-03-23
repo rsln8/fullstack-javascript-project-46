1b2a984b1584352d5abe9f2e8d728d2b8dcbf1c0
Anyakin <aninasvi@gmail.com>
2026-03-23 16:58:00 +0300

    fix: use simple canonical stylish formatter

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -4,32 +4,28 @@ const stringify = (value, depth) => {
   if (!_.isObject(value)) {
     return value;
   }
-  // Увеличиваем отступ на 2 пробела для каждого уровня
-  const indent = '  '.repeat(depth + 2);
-  const bracketIndent = '  '.repeat(depth + 1);
+  const indent = '  '.repeat(depth + 1);
+  const bracketIndent = '  '.repeat(depth);
   const lines = Object.entries(value).map(([key, val]) => `${indent}  ${key}: ${stringify(val, depth + 1)}`);
   return `{\n${lines.join('\n')}\n${bracketIndent}}`;
 };
 
 const stylish = (diffTree, depth = 0) => {
+  const indent = '  '.repeat(depth);
   const result = diffTree.map((node) => {
     const key = node.key;
     const value = node.value;
-    const indent = '  '.repeat(depth);
-    const signIndent = depth === 0 ? '  ' : '  '.repeat(depth + 2);
-    const unchangedIndent = depth === 0 ? '    ' : '  '.repeat(depth + 3);
-    
     switch (node.type) {
       case 'added':
-        return `${signIndent}+ ${key}: ${stringify(value, depth + 1)}`;
+        return `${indent}  + ${key}: ${stringify(value, depth + 1)}`;
       case 'removed':
-        return `${signIndent}- ${key}: ${stringify(value, depth + 1)}`;
+        return `${indent}  - ${key}: ${stringify(value, depth + 1)}`;
       case 'unchanged':
-        return `${unchangedIndent}${key}: ${stringify(value, depth + 1)}`;
+        return `${indent}    ${key}: ${stringify(value, depth + 1)}`;
       case 'changed':
         return [
-          `${signIndent}- ${key}: ${stringify(node.oldValue, depth + 1)}`,
-          `${signIndent}+ ${key}: ${stringify(node.newValue, depth + 1)}`,
+          `${indent}  - ${key}: ${stringify(node.oldValue, depth + 1)}`,
+          `${indent}  + ${key}: ${stringify(node.newValue, depth + 1)}`,
         ].join('\n');
       case 'nested':
         return `${indent}    ${key}: {\n${stylish(node.children, depth + 1)}\n${indent}    }`;
