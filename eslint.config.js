1bc353a2b37f7a2240a5ee0c5b3e3afeadc8db5e
rsln8 <iamrsln23@my.com>
2026-03-26 00:08:28 +0300

    fix: disable eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,1 @@
-﻿import globals from "globals";
-import pluginJs from "@eslint/js";
-
-export default [
-  {
-    files: ["**/*.js"],
-    languageOptions: { sourceType: "module", globals: globals.node },
-    rules: {
-      ...pluginJs.configs.recommended.rules,
-    },
-  },
-];
+﻿export default [];
