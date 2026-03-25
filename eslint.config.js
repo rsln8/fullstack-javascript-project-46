8658075e12c3855c44fe1ce40e462dfdc11f7a89
rsln8 <iamrsln23@my.com>
2026-03-26 00:05:56 +0300

    fix: simplify eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,36 +1,12 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import jest from 'eslint-plugin-jest';
+﻿import globals from "globals";
+import pluginJs from "@eslint/js";
 
 export default [
-  js.configs.recommended,
   {
-    ignores: ['node_modules/', 'coverage/', '__tests__/__fixtures__/', '**/*.json'],
-  },
-  {
-    files: ['**/*.js'],
-    languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-      globals: {
-        ...globals.node,
-        ...globals.jest,
-      },
-    },
+    files: ["**/*.js"],
+    languageOptions: { sourceType: "module", globals: globals.node },
     rules: {
-      'no-console': 'off',
-      'indent': ['error', 2],
-      'quotes': ['error', 'single'],
-      'semi': ['error', 'always'],
-    },
-  },
-  {
-    files: ['__tests__/**/*.js'],
-    plugins: {
-      jest,
-    },
-    rules: {
-      ...jest.configs.recommended.rules,
+      ...pluginJs.configs.recommended.rules,
     },
   },
 ];
