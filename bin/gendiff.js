5849071cf2f6c380502ca562071daf4b3b7166fc
Anyakin <aninasvi@gmail.com>
2026-03-25 14:38:48 +0300

    linteragain

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-const { Command } = require('commander');
-const genDiff = require('../src/index.js');
+import { Command } from 'commander';
+import genDiff from '../src/index.js';
 
 const program = new Command();
 
@@ -9,18 +9,13 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .option('-f, --format [type]', 'output format', 'stylish')
+  .option('-f, --format [type]', 'output format')
   .argument('<filepath1>')
   .argument('<filepath2>')
   .action((filepath1, filepath2, options) => {
     const format = options.format || 'stylish';
-    try {
-      const diff = genDiff(filepath1, filepath2, format);
-      console.log(diff);
-    } catch (error) {
-      console.error('Error:', error.message);
-      process.exit(1);
-    }
+    const result = genDiff(filepath1, filepath2, format);
+    console.log(result);
   });
 
 program.parse();
