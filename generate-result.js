6dd936c18a6a7e475b80355c92ee4a4375ea7fd3
rsln8 <iamrsln23@my.com>
2026-03-26 23:21:04 +0300

    все тесты проходят, линтер чист

--- generate-result.js
+++
@@ -1,5 +0,0 @@
-import genDiff from './src/index.js';
-import fs from 'fs';
-
-const result = genDiff('__tests__/__fixtures__/nested1.json', '__tests__/__fixtures__/nested2.json', 'json');
-fs.writeFileSync('__tests__/__fixtures__/result_nested_json.txt', result, 'utf8');
