e7658be8c2fb6735c5cbd4177a561137b0221d3b
rsln8 <iamrsln23@my.com>
2026-03-26 23:17:07 +0300

    ya uje ustal

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -3,20 +3,20 @@ import fs from 'fs';
 import path from 'path';
 import genDiff from '../src/index.js';
 
-const getFixturePath = filename => path.join(process.cwd(), '__fixtures__', filename);
+const getFixturePath = filename => path.join(process.cwd(), '__tests__', '__fixtures__', filename);
 const readFile = filename => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 describe('gendiff', () => {
   const formats = ['json', 'yml', 'yaml'];
 
-  test.each(formats)('flat %s', (format) => {
+  test.each(formats)('flat %s', format => {
     const file1 = `file1.${format}`;
     const file2 = `file2.${format}`;
     const result = readFile('result_stylish.txt');
     expect(genDiff(getFixturePath(file1), getFixturePath(file2), 'stylish')).toBe(result);
   });
 
-  test.each(formats)('nested %s', (format) => {
+  test.each(formats)('nested %s', format => {
     const file1 = `nested1.${format}`;
     const file2 = `nested2.${format}`;
     const result = readFile('result_nested_stylish.txt');
